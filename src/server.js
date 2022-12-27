9474eb056a38243b7448328db94d5699fd884a19
SGT SEO <vpflt992@gmail.com>
2022-12-27 20:52:48 +0900

    Commit commit

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
-import express from "express"
+import express from "express";
+import WebSocket from "ws";
+import http from "http";
 
 const app = express();
 
@@ -7,14 +9,34 @@ app.set("view engine","pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname +"/public"))
 
-app.get("/", (req,res) => res.render("home"));
+app.get("/", (_,res) => res.render("home"));
+app.get("/*",(_,res)=> res.redirect("/"));
 
 /*
 const handleListen = () => console.log(`Listening on Http : } `)
 console.log("Hello")
 
 app.listen(3000);
+
+app.listen(3000, handleListen);
 */
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+//Can Access Server as a variable
+const server = http.createServer(app);
+
+//By pass the server as parameter, we can run both http and WebSocket both
+// * not a neccessary, but if you want to run 2 server at once
+const wss = new WebSocket.Server({server})
+
+
+wss.on("connection",(socket) => {
+    socket.on("close",() => console.log("Disconnected from Browswer"))
+    socket.on("message", (message)=>{
+        console.log(message)
+    })
+    console.log(socket);
+});
+
+server.listen(3000,handleListen);
